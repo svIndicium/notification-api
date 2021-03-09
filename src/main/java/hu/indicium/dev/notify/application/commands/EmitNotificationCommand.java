6fa5488ddfc8a6c8b8f889368221b1ce757457cd
joostlek <joostlek@outlook.com>
2021-03-09 15:46:24 +0100

    Create Builder for EmitNotificationCommand

--- src/main/java/hu/indicium/dev/notify/application/commands/EmitNotificationCommand.java
+++ src/main/java/hu/indicium/dev/notify/application/commands/EmitNotificationCommand.java
@@ -1,8 +1,10 @@
 package hu.indicium.dev.notify.application.commands;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class EmitNotificationCommand {
     private String message;
 }
