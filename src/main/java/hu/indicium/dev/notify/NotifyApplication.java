1d0daaa357850f7b9a19bff5e70db77b0c3c5987
joostlek <joostlek@outlook.com>
2021-03-09 15:46:37 +0100

    Add ObjectMapper config

--- src/main/java/hu/indicium/dev/notify/NotifyApplication.java
+++ src/main/java/hu/indicium/dev/notify/NotifyApplication.java
@@ -1,13 +1,23 @@
 package hu.indicium.dev.notify;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NotifyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotifyApplication.class, args);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        return objectMapper;
     }
 
 }
