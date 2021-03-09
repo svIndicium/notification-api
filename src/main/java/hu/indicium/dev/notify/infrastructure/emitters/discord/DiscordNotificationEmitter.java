0c39827b5df16118ef5751c68b3aec8374acb173
joostlek <joostlek@outlook.com>
2021-03-09 13:44:31 +0100

    Change DiscordNotificationEmitter in Springless object

--- src/main/java/hu/indicium/dev/notify/infrastructure/emitters/discord/DiscordNotificationEmitter.java
+++ src/main/java/hu/indicium/dev/notify/infrastructure/emitters/discord/DiscordNotificationEmitter.java
@@ -8,11 +8,9 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Component
 @Slf4j
 public class DiscordNotificationEmitter implements NotificationEmitter {
     @Override
